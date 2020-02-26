ce2b6a987e7ca2810c7d19e314e3641542adf0ee
Matthew Mauney <mauney@users.noreply.github.com>
2020-02-25 21:40:04 -0500

    Implement stack

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -27,10 +27,10 @@ MUL  = 0b10100010  #  2
 # NOP  = 0b00000000  #  0
 # NOT  = 0b01101001  #  9
 # OR   = 0b10101010  # 10
-# POP  = 0b01000110  #  6
+POP  = 0b01000110  #  6
 # PRA  = 0b01001000  #  8
 PRN  = 0b01000111  #  7
-# PUSH = 0b01000101  #  5
+PUSH = 0b01000101  #  5
 # RET  = 0b00010001  #  1
 # SHL  = 0b10101100  # 12
 # SHR  = 0b10101101  # 13
@@ -55,7 +55,11 @@ class CPU:
         self.branchtable[HLT] = self.hlt
         self.branchtable[LDI] = self.ldi
         self.branchtable[MUL] = self.mul
+        self.branchtable[POP] = self.pop
         self.branchtable[PRN] = self.prn
+        self.branchtable[PUSH] = self.push
+        # initialize the stack pointer
+        self.sp = 0xF4
 
     def ram_read(self, MAR):
         return self.ram[MAR]
@@ -78,9 +82,27 @@ class CPU:
         self.alu('MUL', operand_a, operand_b)
         self.pc += 3
 
+    def pop(self):
+        if self.sp > 0xF3:
+            print('Error: the stack is empty')
+            sys.exit(3)
+        else:
+            operand = self.ram_read(self.pc + 1)
+            self.reg[operand] = self.ram[self.sp]
+            self.sp += 1
+            self.pc += 2
+
     def prn(self):
         operand = self.ram_read(self.pc + 1)
         print(self.reg[operand])
+        self.pc += 2
+
+    def push(self):
+        # move to the next position in the stack
+        self.sp -= 1
+        # assign the value
+        operand = self.ram_read(self.pc + 1)
+        self.ram[self.sp] = self.reg[operand]
         self.pc += 2
 
     def load(self, program):
@@ -144,5 +166,5 @@ class CPU:
             if ir in self.branchtable:
                 self.branchtable[ir]()
             else:
-                print(f"I did not understand that ir: {ir}")
+                print(f"I did not understand that ir: {ir:b}")
                 sys.exit(1)
