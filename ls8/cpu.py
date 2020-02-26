04c1361d7a3956b63beb4959fd1c877105b6164e
Matthew Mauney <mauney@users.noreply.github.com>
2020-02-25 23:43:51 -0500

    Implement Subroutine Calls

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -3,9 +3,9 @@
 import sys
 
 # Instructions
-# ADD  = 0b10100000  #  0
+ADD  = 0b10100000  #  0
 # AND  = 0b10101000  #  8
-# CALL = 0b01010000  #  0
+CALL = 0b01010000  #  0
 # CMP  = 0b10100111  #  7
 # DEC  = 0b01100110  #  6
 # DIV  = 0b10100011  #  3
@@ -18,7 +18,7 @@ HLT  = 0b00000001  #  1
 # JGT  = 0b01010111  #  7
 # JLE  = 0b01011001  #  9
 # JLT  = 0b01011000  #  8
-# JMP  = 0b01010100  #  4
+JMP  = 0b01010100  #  4
 # JNE  = 0b01010110  #  6
 # LD   = 0b10000011  #  3
 LDI  = 0b10000010  #  2
@@ -31,7 +31,7 @@ POP  = 0b01000110  #  6
 # PRA  = 0b01001000  #  8
 PRN  = 0b01000111  #  7
 PUSH = 0b01000101  #  5
-# RET  = 0b00010001  #  1
+RET  = 0b00010001  #  1
 # SHL  = 0b10101100  # 12
 # SHR  = 0b10101101  # 13
 # ST   = 0b10000100  #  4
@@ -46,20 +46,28 @@ class CPU:
         """Construct a new CPU."""
         # pc represents the Program Counter
         self.pc = 0
+        # initialize the stack pointer
+        self.sp = 0xF4
         # reg represents the eight general purpose registers
         self.reg = [0] * 8
         # ram represents 256 bytes of random access memory
         self.ram = [0] * 256
+        # operands
+        self.operand_a = None
+        self.operand_b = None
+        self.num_operands = None
         # branch table
         self.branchtable = {}
+        self.branchtable[ADD] = self.add
+        self.branchtable[CALL] = self.call
         self.branchtable[HLT] = self.hlt
+        self.branchtable[JMP] = self.jmp
         self.branchtable[LDI] = self.ldi
         self.branchtable[MUL] = self.mul
         self.branchtable[POP] = self.pop
         self.branchtable[PRN] = self.prn
         self.branchtable[PUSH] = self.push
-        # initialize the stack pointer
-        self.sp = 0xF4
+        self.branchtable[RET] = self.ret
 
     def ram_read(self, MAR):
         return self.ram[MAR]
@@ -67,43 +75,53 @@ class CPU:
     def ram_write(self, MDR, MAR):
         self.ram[MAR] = MDR
 
+    def add(self):
+        self.alu('ADD', self.operand_a, self.operand_b)
+        self.pc += (self.num_operands + 1)
+
+    def call(self):
+        # push pc + 2 onto the stack
+        self.sp -= 1
+        self.ram[self.sp] = self.pc + 2
+        self.pc = self.reg[self.operand_a]
+
     def hlt(self):
         sys.exit(0)
 
+    def jmp(self):
+        self.pc = self.reg[self.operand_a]
+
     def ldi(self):
-        operand_a = self.ram_read(self.pc + 1)
-        operand_b = self.ram_read(self.pc + 2)
-        self.reg[operand_a] = operand_b
-        self.pc += 3
+        self.reg[self.operand_a] = self.operand_b
+        self.pc += (self.num_operands + 1)
 
     def mul(self):
-        operand_a = self.ram_read(self.pc + 1)
-        operand_b = self.ram_read(self.pc + 2)
-        self.alu('MUL', operand_a, operand_b)
-        self.pc += 3
+        self.alu('MUL', self.operand_a, self.operand_b)
+        self.pc += (self.num_operands + 1)
 
     def pop(self):
         if self.sp > 0xF3:
             print('Error: the stack is empty')
             sys.exit(3)
         else:
-            operand = self.ram_read(self.pc + 1)
-            self.reg[operand] = self.ram[self.sp]
+            self.reg[self.operand_a] = self.ram[self.sp]
             self.sp += 1
-            self.pc += 2
+            self.pc += (self.num_operands + 1)
 
     def prn(self):
-        operand = self.ram_read(self.pc + 1)
-        print(self.reg[operand])
-        self.pc += 2
+        print(self.reg[self.operand_a])
+        self.pc += (self.num_operands + 1)
 
     def push(self):
         # move to the next position in the stack
         self.sp -= 1
         # assign the value
-        operand = self.ram_read(self.pc + 1)
-        self.ram[self.sp] = self.reg[operand]
-        self.pc += 2
+        self.ram[self.sp] = self.reg[self.operand_a]
+        self.pc += (self.num_operands + 1)
+
+    def ret(self):
+        self.pc = self.ram[self.sp]
+        self.sp += 1
 
     def load(self, program):
         """Load a program into memory."""
@@ -162,6 +180,13 @@ class CPU:
         while True:
             # ir represents the Instruction Register
             ir = self.ram_read(self.pc)
+
+            self.num_operands = ir >> 6
+            if self.num_operands == 1:
+                self.operand_a = self.ram_read(self.pc + 1)
+            elif self.num_operands == 2:
+                self.operand_a = self.ram_read(self.pc + 1)
+                self.operand_b = self.ram_read(self.pc + 2)
 
             if ir in self.branchtable:
                 self.branchtable[ir]()
